9e37fde34cda0206ef21f9d1075a0f0a8098cffd
KyrusGaming <jwarrocks@yahoo.com>
2017-10-20 08:40:14 -0400

    Update config.js

--- sleekLoad2/js/config.js
+++ sleekLoad2/js/config.js
@@ -89,8 +89,7 @@ var l_musicDisplay = true;
 Place how much .ogg songs you want
 (Put the files in the songs folder)*/
 var l_musicPlaylist = [
-	{ogg: "lullaby.ogg", name: "Lullaby of Deserted Hell"},
-	{ogg: "falls_in_fall.ogg", name: "秋に舞う椛と滝飛沫"},
+  	{ogg: "videoplayback.ogg", name: "Darth Vaders March"},
 	{youtube: "zA6wRutN_7Q", name: "Suwa-cafeteria"},
 	{youtube: "KtlKbE4qU0c", name: "月の妖鳥、化猫の幻"},
 ];
